0543f0624fe121464025dc728134552d2c42bd1d
G-flame <thegreenflame1507@gmail.com>
2025-05-22 11:25:22 +0530

    Makeing it more ok?

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 console.log("The old website was too slow and had very bad code so i will rewrite the website ! ~G-flame")
-console.log("https://g-flame.github.io/G-flame/old-website/index.html")
+console.log("https://g-flame.github.io/G-flame/old-website/")
